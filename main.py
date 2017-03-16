13cc52b9a96639c2066ea13c942c8da1710acb43
Liu Shuo <liu@shuo.email>
2017-03-16 13:59:54 +0800

    output to file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import bs4 as bs
 import urllib.request
 from string import Template
 import sys
+import os
+from datetime import datetime
 
 address = Template('https://www.cgtn.com/news/section.do?curPage=0&category=$category')
 # print('Scraping Page: ', address.substitute(category=sys.argv[1]))
@@ -21,5 +23,18 @@ for div in soup.find_all('div',class_='title'):
                 data['time'] = child.p.string
     output.append(data)
 
+now = datetime.now()
+
+DateFolderName = str(now.year) + '-' + str(now.month).zfill(2) + '-' + str(now.day).zfill(2)
+if not os.path.exists(DateFolderName):
+    os.makedirs(DateFolderName)
+
+HourFolderName = DateFolderName + '/' + str(now.hour).zfill(2)
+if not os.path.exists(HourFolderName):
+    os.makedirs(HourFolderName)
+
 for data in output:
-    print(data['time'],',',data['title'],',',data['link'])
+    # print(data['time'],',',data['title'],',',data['link'])
+    ArticleFileName = HourFolderName + '/' + data['title']
+    with open(ArticleFileName, 'w') as f:
+        f.write(data['time'])
